1af07dde06e03f5933b6a2a6a3f6c184acf6f42f
Shunmugapriya S <sshunmugapriya49@gmail.com>
2026-07-30 14:44:04 +0530

    Time: 1735 ms (21.07%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,3 +1,10 @@
+-- with reference as (select
+-- case 
+--    when income<20000 then 'Low Salary'
+--    when (income >=20000 and income<=50000) then 'Average Salary'
+--    else "High Salary"
+-- end  as category from Accounts)
+-- select category,count(*) as accounts_count from reference group by category;
 select 'Low Salary' as category,sum(income<20000) as accounts_count from Accounts
 union all 
 select 'Average Salary' as category ,sum(income>=20000 and income<=50000) as accounts_count from Accounts
