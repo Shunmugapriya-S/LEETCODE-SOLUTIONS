a53476e59ec9fb75deb16e410e817cc6abe89a79
Shunmugapriya S <sshunmugapriya49@gmail.com>
2026-08-02 00:00:25 +0530

    Time: 534 ms (95.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,6 @@
+with cte as (
+select * ,
+    count(*) over(partition by tiv_2015)as cnt_tiv,
+    count(*) over(partition by lat,lon) as crt_loc
+from Insurance)
+select round(sum(tiv_2016),2) as tiv_2016 from cte where cnt_tiv>1 and crt_loc=1;
