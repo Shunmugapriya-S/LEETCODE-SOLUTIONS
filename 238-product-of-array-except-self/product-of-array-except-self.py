74897de5256a1f372f594b23df3a18efa69f3f2e
Shunmugapriya S <sshunmugapriya49@gmail.com>
2026-08-24 23:39:49 +0530

    Time: 35 ms (25.52%) | Memory: 27.7 MB (13.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,9 +3,11 @@ class Solution:
         n=len(nums)
         l=[1]*n
         r=[1]*n
-        res=0
+        res=[]
         for i in range(1,n):
             l[i]=l[i-1]*nums[i-1]
         for i in range(n-2,-1,-1):
             r[i]=r[i+1]*nums[i+1]
-        return[l[i]*r[i] for i in range(n)]
+        for i in range(n):
+            res.append(l[i]*r[i])
+        return res
