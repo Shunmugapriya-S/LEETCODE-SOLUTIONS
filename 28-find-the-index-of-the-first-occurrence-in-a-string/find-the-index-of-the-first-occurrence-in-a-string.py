3b479c0059737e68b116a8e9f78077a3e358a61d
Shunmugapriya S <sshunmugapriya49@gmail.com>
2026-08-26 01:05:06 +0530

    Time: 2 ms (20.91%) | Memory: 19.5 MB (8.44%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,11 +1,30 @@
-class Solution:
+class Solution:## kmp 
     def strStr(self, haystack: str, needle: str) -> int:
         if needle=="":
             return 0
-        for i in range(len(haystack)+1-len(needle)):
-            for j in range(len(needle)):
-                if haystack[i+j]!=needle[j]:
-                    break
-                if j==len(needle)-1:
-                    return i
+        lps=[0]*len(needle)
+        prevlps,i=0,1
+        while i<len(needle):
+            if needle[i]==needle[prevlps]:
+                prevlps+=1
+                lps[i]=prevlps
+                i+=1
+            elif prevlps==0:
+                lps[i]=0
+                i+=1
+            else:
+                prevlps=lps[prevlps-1]
+        i=0
+        j=0
+        while i<len(haystack):
+            if haystack[i]==needle[j]:
+                i,j=i+1,j+1
+                if j==len(needle):
+                        return i-len(needle)
+            else:
+                if j==0:
+                    i+=1
+                else:
+                    j=lps[j-1]
+                    
         return -1
